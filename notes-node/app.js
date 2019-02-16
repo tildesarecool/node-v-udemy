ba95e013b1291dd446fd32002990ae069e2c4187
Keith <tildesare.cool@gmail.com>
2019-02-15 19:19:38 -0800

    further progress on notes app

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,11 @@
 console.log("Starting app.");
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFileSync('greetings.txt', 'Hello world');
+var user = os.userInfo();
+
+// this is used in modern versions of node instead of just 'appendFile'
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+
+//console.log(user);
