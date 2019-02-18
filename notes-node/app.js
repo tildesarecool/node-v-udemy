638b15e13bb7161fe97381c09255eff11f3a529b
tildesarecool <tildesare.cool@gmail.com>
2019-02-17 18:18:11 -0800

    added some functions for CLI args

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,6 +2,7 @@ console.log("Starting app.js");
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes');
 
@@ -10,21 +11,34 @@ const notes = require('./notes');
 // var filteredArray = _.uniq(['Mike']);
 // console.log(filteredArray);
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = argv._[0];
 console.log('Command: ', command);
-console.log(process.argv);
+// console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-    console.log('Adding new note');
+    //console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
+
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    //console.log('Listing all notes');
+    notes.getAll(); //return all notes regardless of title
 // adding two more else/clauses on my own: read and remove
+
 } else if (command === 'read') {
-    console.log('Reading note'); // showing one single note
+    //console.log('Reading note'); // showing one single note
+    // will require title as it's showing a single note
+    notes.getNote(argv.title);
+
+
 } else if (command === 'remove') {
-    console.log('removing note');
+    //console.log('removing note');
+    notes.removeNote(argv.title);
+
 } else {
     console.log("Command not recognized");
+
 }
 
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,35 @@
 console.log('Starting notes.js');
 
-module.exports.addNote =  () => {
-    console.log('addNote');
-    return'New Note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 };
+
+var getAll = () => {
+    console.log("Getting all notes");
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+}
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+}
+
+module.exports = {
+    addNote, //: addNote // same as addnNote: addNote. by itself is an ES6 thing
+    getAll,
+    getNote,
+    removeNote
+};
+
+//getnote function
+//removnote passing in note want to remove 
+
+
+
+// module.exports.addNote =  () => {
+//     console.log('addNote');
+//     return'New Note';
+// };
 
