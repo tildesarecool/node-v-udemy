2ca4f949a8c3ca50e77777e93d28c36f1501a0a7
tildesarecool <tildesare.cool@gmail.com>
2019-02-20 19:06:17 -0800

    utilzing try/catch in case json doesn't exist

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+   // console.log('Adding note', title, body);
+   var notes = [];
+   var note = {
+       title, // es6 style
+       body
+   };
+
+   try {
+    var notesString = fs.readFileSync('notes-data.json')
+    notes = JSON.parse(notesString);
+   } catch(e) {
+
+   }
+
+
+
+   notes.push(note);
+   fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var getAll = () => {
